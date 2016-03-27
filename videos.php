7ac5623ea5a3d337f98fed02afa9ca1ec51a6195
Jstnwrds55 <Jstnwrds55@gmail.com>
2016-03-26 19:47:35 -0600

    Added Site
    Added full crown the empire site to repo

---
+++ videos.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="shortcut icon" href="favicon.ico">
+    <title>Crown The Empire</title>
+    <link href='http://fonts.googleapis.com/css?family=Ranga' rel='stylesheet' type='text/css'>
+    <link href="http://s3.amazonaws.com/codecademy-content/courses/ltp/css/shift.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+
+</head>
+
+<body>
+    <nav class="fixed-nav-bar" id="navbar">
+        <div class="nav">
+            <div class="container">
+                <!--<ul class="pull-left">
+          
+            <li><a href="index.html">Home</a></li>
+            <li><a href="videos.html">Videos</a></li>
+          </ul>-->
+
+                <ul class="logo">
+                    <li>
+                        <a href="index.php">
+                            <img src="logo-invert.png" onmouseover="this.src='logo.png'" onmouseout="this.src='logo.png'" />
+                        </a>
+                    </li>
+                </ul>
+                <ul class="pull-right">
+                    <li><a href="videos.php">Videos</a>
+                    </li>
+                    <li><a href="dates.php">Tour Dates</a>
+                    </li>
+                    <li><a href="#">Help</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="videos">
+        <div class="container">
+            <h1>Videos</h1>
+            <p>Here are the music videos that Crown the Empire has put out</p>
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Satellites / Rise of the Runaways</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/o2Wf5ni9FWI" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Bloodline</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/QrmeLlhHJ8g" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Memories of a Broken Heart</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/bwFmrjuZJ-I" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Machines</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/IVLOa592jgk" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Oh, Catastrophe</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/spCXrPXbRJw" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>The Fallout</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/Jvuv_XEZE7o" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Initiation</h3>
+                        <iframe width="100%" height="315" src="https://www.youtube.com/embed/sLPesxVAysg" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="thumbnail">
+                        <h3>Prisoners of War</h3>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/ew76X3CK_oo" frameborder="0" allowfullscreen></iframe>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-3">
+                </div>
+
+                <div class="col-md-6">
+                    <h3>Cross Our Bones</h3>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/xV_G6TcKkAk" frameborder="0" allowfullscreen></iframe>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <div class="learn-more">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4">
+                    <h3>Rise Records</h3>
+                    <p>Check out similar bands signed to Rise Records at the official website.</p>
+                    <p><a href="http://www.riserecords.com">Visit their website</a>
+                    </p>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>Contact</h3>
+                    <p>Need to contact Crown The Empire? Feel free to reach out through social media.</p>
+                    <p><a href="https://www.facebook.com/Crowntheempire">Facebook</a>  <a href="https://twitter.com/CrownTheEmpire">Twitter</a></a>
+                    </p>
+                </div>
+
+                <div class="col-md-4">
+                    <h3>Merchandise</h3>
+                    <p>Feel free to get some sweet Crown The Empire related clothing and attire from their merchandise website.</p>
+                    <p><a href="https://ctemerch.com/">Visit it now</a>
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
